8d6315815a34e58f8ca8bcc277db0a2c0c20af8a
Ashum Bhuju Shrestha <136584855+rudra100008@users.noreply.github.com>
2024-11-27 17:19:28 +0545

    Update ChatAppApplication.java

--- chatApp/src/main/java/com/chatApp/ChatAppApplication.java
+++ chatApp/src/main/java/com/chatApp/ChatAppApplication.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.chatApp;
 
 import org.springframework.boot.CommandLineRunner;
@@ -16,22 +16,4 @@ public class ChatAppApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 	}
 }
-=======
-package com.chatApp;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ChatAppApplication implements CommandLineRunner {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ChatAppApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-	}
-}
->>>>>>> 7273ec07c4883cbb93cb3253b8cb73155376d025
