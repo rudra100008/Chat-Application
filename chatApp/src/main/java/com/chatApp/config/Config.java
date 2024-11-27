e6bb7eda6d430ffd6fe56db8e8d1b9f14ffad67f
Ashum Bhuju Shrestha <136584855+rudra100008@users.noreply.github.com>
2024-11-27 17:19:00 +0545

    Update Config.java

--- chatApp/src/main/java/com/chatApp/config/Config.java
+++ chatApp/src/main/java/com/chatApp/config/Config.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.chatApp.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -21,27 +21,4 @@ public class Config implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
-=======
-package com.chatApp.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-@Configuration
-@EnableWebSocketMessageBroker
-public class Config implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/server1").setAllowedOrigins("http://localhost:3000").withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
-    }
-}
->>>>>>> 7273ec07c4883cbb93cb3253b8cb73155376d025
